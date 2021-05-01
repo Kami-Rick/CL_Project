51c083e7f70fcd43e366d21cf1c392e002e3752b
Adok6 <80463620+Adok6@users.noreply.github.com>
2021-05-01 23:05:22 +0600

    Add files via upload

---
+++ reg-up.php
@@ -0,0 +1,21 @@
+<?php 
+require_once 'db_php';
+
+$uname = trim($_POST['uname']);
+$email_reg = trim($_POST['email-reg']);
+$pwd = trim($_POST['pwd']);
+
+if (!empty($uname) && !empty($email_reg) && !empty($pwd) ) {
+
+	$pwd = password_hash($pwd, PASSWORD_DEFAULT);
+
+	$sql = 'insert into users(nickname, email, password) values (:uname, email-reg, pwd)';
+	$params = ['uname' => $uname, 'email-reg' => $email_reg, 'pwd' => $pwd];
+
+	$stmt = $pdo -> prepare($sql);
+	$stmt -> execute($params);
+
+	echo 'Successfully registered';
+} else {
+	echo 'Fullfil all boxes';
+}
