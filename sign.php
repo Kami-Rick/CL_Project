155e4c97b8ef7c0804e9cdbdde40849d83c85934
Adok6 <80463620+Adok6@users.noreply.github.com>
2021-05-01 16:00:44 +0600

    Add files via upload

---
+++ sign.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sign up / Sign in</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="sign_style.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+	<script type="text/javascript" src="sign_func.js" defer></script>
+	<script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    
+</head>
+<body>
+
+	<div class="content-sign">
+        <div class="container-sign">
+            <div class="header-sign">
+                <h2 style="color: black">Create Account</h2>
+            </div>
+            <form id="form-sign" class="form-sign">
+                <div class="form-control-sign">
+                    <label for="username">Username</label>
+                    <input type="text" placeholder="username" id="username" />
+                    <i class="fas fa-check-circle"></i>
+                    <i class="fas fa-exclamation-circle"></i>
+                    <p><small>Error message</small></p>
+                </div>
+                <div class="form-control-sign">
+                    <label for="username">Email</label>
+                    <input type="email" placeholder="example@gmail.com" id="email" />
+                    <i class="fas fa-check-circle"></i>
+                    <i class="fas fa-exclamation-circle"></i>
+                    <p><small>Error message</small></p>
+                </div>
+                <div class="form-control-sign">
+                    <label for="username">Password</label>
+                    <input type="password" placeholder="Password" id="password" />
+                    <i class="fas fa-check-circle"></i>
+                    <i class="fas fa-exclamation-circle"></i>
+                    <p><small>Error message</small></p>
+                </div>
+                <div class="form-control-sign">
+                    <label for="username">Password Confirmation</label>
+                    <input type="password" placeholder="Password Confirmation" id="password2" />
+                    <i class="fas fa-check-circle"></i>
+                    <i class="fas fa-exclamation-circle"></i>
+                    <p><small>Error message</small></p>
+                </div>
+                <button class="btn btn-outline-dark sign-btn w-100" type="submit">Sign Up</button>
+                <div class="d-flex justify-content-center">
+                    <p class="mt-3">If You already have an account <a href="#" id="login-toggler">Log In</a></p>
+                </div>
+            </form>
+        </div>
+        <div class="container-login d-none">
+            <div class="header-login">
+                <h2 style="color: black">Enter your account</h2>
+            </div>
+            <form id="form-login" class="form-login">
+
+                <div class="form-control-login">
+                    <label for="username">Email</label>
+                    <input type="email" placeholder="example@gmail.com" id="email-login" />
+                    <i class="fas fa-check-circle"></i>
+                    <i class="fas fa-exclamation-circle"></i>
+                    <p><small>Error message</small></p>
+                </div>
+                <div class="form-control-login">
+                    <label for="username">Password</label>
+                    <input type="password" placeholder="Password" id="password-login" />
+                    <i class="fas fa-check-circle"></i>
+                    <i class="fas fa-exclamation-circle"></i>
+                    <p><small>Error message</small></p>
+                </div>
+                <button class="btn btn-outline-dark sign-btn w-100" type="submit">Sign In</button>
+                <div class="d-flex justify-content-center">
+                    <p class="mt-3">If You don't have an account <a href="#" id="sign-toggler">Sign Up</a></p>
+                </div>
+            </form>
+        </div>
+    </div>
+
+</body>
+</html>
---
+++ site_description.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Site description</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- ===== BOX ICONS ===== -->
+    <link href='https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css' rel='stylesheet'>
+    <!-- ===== BOOTSTRAP 4 ===== -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+    <!-- ===== CSS ===== -->
+    <link rel="stylesheet" href="styles.css">
+
+</head>
+<body id="body-pd">
+<header class="header" id="header">
+    <div class="header__toggle">
+        <i class='bx bx-menu' id="header-toggle"></i>
+    </div>
+
+    <div class="header__img">
+        <img src="perfil.jpg" alt="">
+    </div>
+</header>
+
+<div class="l-navbar" id="nav-bar">
+    <nav class="nav">
+        <div>
+            <a href="#" class="nav__logo">
+                <i class='bx bx-layer nav__logo-icon'></i>
+                <span class="nav__logo-name">Business helper</span>
+            </a>
+
+            <div class="nav__list">
+                <a href="#" class="nav__link active">
+                    <i class='bx bx-grid-alt nav__icon' ></i>
+                    <span class="nav__name">Hypotheses</span>
+                </a>
+
+                <a href="#" class="nav__link">
+                    <i class='bx bx-user nav__icon' ></i>
+                    <span class="nav__name">Co-worker</span>
+                </a>
+
+                <a href="#" class="nav__link">
+                    <i class='bx bx-message-square-detail nav__icon' ></i>
+                    <span class="nav__name">Library of hypotheses</span>
+                </a>
+
+                <a href="#" class="nav__link">
+                    <i class='bx bx-bookmark nav__icon' ></i>
+                    <span class="nav__name">Traffic</span>
+                </a>
+
+                <a href="#" class="nav__link">
+                    <i class='bx bx-folder nav__icon' ></i>
+                    <span class="nav__name">Finance</span>
+                </a>
+
+                <a href="#" class="nav__link">
+                    <i class='bx bx-bar-chart-alt-2 nav__icon' ></i>
+                    <span class="nav__name">Analytics</span>
+                </a>
+                
+                <a href="sign.html" class="nav__link">
+                    <i class='bx bx-right-arrow nav__icon'></i>
+                    <span class="nav__name">Register</span>
+                </a>
+            </div>
+        </div>
+
+        <a href="#" class="nav__link">
+            <i class='bx bx-log-out nav__icon' ></i>
+            <span class="nav__name">Log Out</span>
+        </a>
+    </nav>
+</div>
+
+<div class="container rounded shadow p-3 mb-5 bg-white rounded" style="border:2px solid lavenderblush">
+
+    <div class="row">
+        <div class="col-sm-12 box">
+            <p style="font-family: 'Verdana'">
+            <p style="font-size: 18px"><b>How to work with the platform?</b></p>
+            <br>
+            <p style="font-size: 14px">Watch a short tutorial video or learn the platform actions and their order below:</p>
+            <br>
+            <p style="font-size: 14px">1. Some advise to work with platform</p>
+            <p style="font-size: 14px">2. Some advise to work with platform</p>
+            <p style="font-size: 14px">3. Some advise to work with platform</p>
+            <p style="font-size: 14px">4. Some advise to work with platform</p>
+            <p style="font-size: 14px">5. Some advise to work with platform</p>
+            <p style="font-size: 14px">6. Some advise to work with platform</p>
+            </p>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-sm-3 box  rounded shadow p-3 mb-5 bg-white rounded" style="border:2px solid lavenderblush ; margin-right: 45px;margin-left: 45px ">
+            <p style="font-family: 'Verdana'">
+            <p style="font-size: 18px"><b>Settings</b></p>
+            <p style="color: dimgrey">Be sure to fill out information about yourself and your business.</p>
+            <br>
+            <p><a class="more" href="#">More details</a> </p>
+            </p>
+        </div>
+        <div class="col-sm-3 box  rounded shadow p-3 mb-5 bg-white rounded" style="border:2px solid lavenderblush ;margin-right: 45px;margin-left: 45px">
+            <p style="font-family: 'Verdana'">
+            <p style="font-size: 18px"><b>Hypotheses</b></p>
+            <p style="color: dimgrey">Conjecture, test, and record results to streamline your business objectives.</p>
+            <p><a class="more" href="#">More details</a> </p>
+            </p>
+        </div>
+        <div class="col-sm-3 box  rounded shadow p-3 mb-5 bg-white rounded" style="border:2px solid lavenderblush ; margin-right: 45px;margin-left: 45px ">
+            <p style="font-family: 'Verdana'">
+            <p style="font-size: 18px"><b>Settings</b></p>
+            <p style="color: dimgrey">Be sure to fill out information about yourself and your business.</p>
+            <br>
+            <p><a class="more" href="#">More details</a> </p>
+            </p>
+        </div>
+        <div class="col-sm-3 box  rounded shadow p-3 mb-5 bg-white rounded" style="border:2px solid lavenderblush ;margin-right: 45px;margin-left: 45px">
+            <p style="font-family: 'Verdana'">
+            <p style="font-size: 18px"><b>Hypotheses</b></p>
+            <p style="color: dimgrey">Conjecture, test, and record results to streamline your business objectives.</p>
+            <p><a class="more" href="#">More details</a> </p>
+            </p>
+        </div>
+        <div class="col-sm-3 box  rounded shadow p-3 mb-5 bg-white rounded" style="border:2px solid lavenderblush ; margin-right: 45px;margin-left: 45px ">
+            <p style="font-family: 'Verdana'">
+            <p style="font-size: 18px"><b>Settings</b></p>
+            <p style="color: dimgrey">Be sure to fill out information about yourself and your business.</p>
+            <br>
+            <p><a class="more" href="#">More details</a> </p>
+            </p>
+        </div>
+        <div class="col-sm-3 box  rounded shadow p-3 mb-5 bg-white rounded" style="border:2px solid lavenderblush ;margin-right: 45px;margin-left: 45px">
+            <p style="font-family: 'Verdana'">
+            <p style="font-size: 18px"><b>Hypotheses</b></p>
+            <p style="color: dimgrey">Conjecture, test, and record results to streamline your business objectives.</p>
+            <p><a class="more" href="#">More details</a> </p>
+            </p>
+        </div>
+    </div>
+</div>
+<!--===== MAIN JS =====-->
+<script src="main.js"></script>
+</body>
+</html>
